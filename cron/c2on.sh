1ca6536fd35e0b184e320e96dbb7ac6761d9a5e0
Vince Loschiavo <vloschiavo@mk802.(none)>
2015-11-30 08:04:05 -0800

    unified the cron jobs into a single script

--- cron/c2on.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-stty -F /dev/ttyACM0 cs8 115200 ignbrk -brkint -icrnl -imaxbel -opost -onlcr -isig -icanon -iexten -echo -echoe -echok -echoctl -echoke noflsh -ixon -crtscts
-tail -f /dev/ttyACM0 &
-sleep 2
-echo "Turning On Outlet 1"
-echo "c21" > /dev/ttyACM0
-killall -9 tail
--- cron/cron-power.sh
+++ cron/cron-power.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 stty -F /dev/ttyACM0 cs8 115200 ignbrk -brkint -icrnl -imaxbel -opost -onlcr -isig -icanon -iexten -echo -echoe -echok -echoctl -echoke noflsh -ixon -crtscts
 tail -f /dev/ttyACM0 &
+echo "$!" > /tmp/a2sleep.pid
 sleep 2
-echo "Turning On Outlet 1"
-echo "a11" > /dev/ttyACM0
-killall -9 tail
+echo "Issuing $1"
+echo "$1" > /dev/ttyACM0
+kill `cat /tmp/a2sleep.pid`
+rm /tmp/a2sleep.pid
